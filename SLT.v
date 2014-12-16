f96569b4918cda7b56518d26b0eba37ce49256cc
Pratool <pratool.gadtaula@students.olin.edu>
2014-12-15 23:13:12 -0500

    Added non-clocked SLT behavioral. Non-functional SLT structural.

--- SLT.v
+++ SLT.v
@@ -1,37 +1,45 @@
-module SLT_behavioral(input clk, input[31:0] input1, input[31:0] input2, output is_true);
+module SLT_behavioral(input[31:0] input1, input[31:0] input2, output res);
 
-reg out;
-
-always @(posedge clk) begin
-if (input1 < input2) begin
-	out = 1;
-end
-end
-
-assign is_true = out;
+assign res = (input1 < input2);
 
 endmodule
 
-//module SLT_structural(input clk, input[31:0] input1, input[31:0] input2, output is_true);
+module SLT_structural(input[31:0] input1, input[31:0] input2, output res);
 
-//endmodule
+wire sub_result[31:0];
+wire carryout;
+wire carryin;
+wire tempXOR[31:0];
+
+assign carryin = 0;
+
+genvar i;
+generate
+for (i = 0; i < 32; i = i + 1) begin: bitslice
+	xor XOR2(tempXOR[i], input2[i], 1'b1);
+	f_adder adder2(sub_result[i], carryout, input1[i], tempXOR[i], carryin);
+	assign carryin = carryout;
+end
+endgenerate
+
+assign res = sub_result[31];
+
+endmodule
 
 module test_SLT();
-reg clk;
-initial clk = 0;
-always #100 clk = !clk;
-
 reg[31:0] input1;
 reg[31:0] input2;
-wire is_true;
+wire res_behavioral;
+wire res_structural;
 
-SLT_behavioral SLT_test(clk, input1, input2, is_true);
+SLT_behavioral SLT_test_behavioral(input1, input2, res_behavioral);
+SLT_structural SLT_test_structural(input1, input2, res_structural);
 
 initial begin
 input1 = 32'd49; #10
 input2 = 32'd67; #10
-#200
-$display("Input1=%d, Input2=%d, is_true=%b", input1, input2, is_true);
+#800
+$display("Input1=%d, Input2=%d, res_behavioral=%b, res_structural=%b", input1, input2, res_behavioral, res_structural);
 end
 
 endmodule
--- test_SLT.do
+++ test_SLT.do
@@ -1,7 +1,13 @@
-vlog -reportprogress 300 -work work SLT.v
+vlog -reportprogress 300 -work work SLT.v adder.v support_modules.v
 vsim -voptargs="+acc" "test_SLT"
 add wave -position insertpoint  \
-sim:test_SLT/SLT_test/is_true 
+sim:test_SLT/res_behavioral \
+sim:test_SLT/res_structural \
+sim:test_SLT/SLT_test_structural/sub_result \
+sim:test_SLT/SLT_test_structural/carryout \
+sim:test_SLT/SLT_test_structural/input1 \
+sim:test_SLT/SLT_test_structural/input2 \
+sim:test_SLT/SLT_test_structural/tempXOR \
 
 run 5000
 wave zoom full
